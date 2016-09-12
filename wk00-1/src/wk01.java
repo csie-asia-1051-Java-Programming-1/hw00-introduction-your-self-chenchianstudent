714179877e6f150e043e600882a78dc3f839e84a
chenchianstudent <105021007@live.asia.edu.tw>
2016-09-12 15:48:25 +0800

    wk00-01 submit

---
+++ wk00-1/src/wk01.java
@@ -0,0 +1,15 @@
+
+public class wk01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("我是陳麒安");
+		System.out.println("CSIE");
+		System.out.println("我的學號是105021007");
+
+	}
+
+}
